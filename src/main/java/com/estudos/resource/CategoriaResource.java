6bdfca2546d6fdbb16ade79fbc1a1033f259e343
Jonas Bernadino <jonas.bernardino@phoebus.local>
2021-02-11 10:23:59 -0300

    PostMapping implementado

--- src/main/java/com/estudos/resource/CategoriaResource.java
+++ src/main/java/com/estudos/resource/CategoriaResource.java
@@ -5,11 +5,10 @@ import com.estudos.domain.dtos.CategoriaDTO;
 import com.estudos.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,8 +30,14 @@ public class CategoriaResource {
         List<Categoria> list = categoriaService.findAll();
         List<CategoriaDTO> listDTO =  list.stream().map(obj ->
                 new CategoriaDTO(obj)).collect(Collectors.toList());
-
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+        obj = categoriaService.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
 
